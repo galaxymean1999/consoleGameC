352394096ad80b14f89e31537e3c4858ba0f17a4
Dominik Daxner <daxndo@ms.spsostrov.cz>
2026-02-26 19:08:30 +0100

    nothing changed

--- engine.c
+++ engine.c
@@ -67,27 +67,26 @@ void drawSprite(int x, int y, int width, int height, char *screenBuff, char *tex
 }
 
 int kbhit(void) {
-  struct termios oldt, newt;
-  int ch;
-  int oldf;
+    struct termios oldt, newt;
+    int ch;
+    int oldf;
+    
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+    
+    ch = getchar();
+    
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
  
-  tcgetattr(STDIN_FILENO, &oldt);
-  newt = oldt;
-  newt.c_lflag &= ~(ICANON | ECHO);
-  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+    if(ch != EOF) {
+        ungetc(ch, stdin);
+        return 1;
+    }
  
-  ch = getchar();
- 
-  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-  fcntl(STDIN_FILENO, F_SETFL, oldf);
- 
-  if(ch != EOF)
-  {
-    ungetc(ch, stdin);
-    return 1;
-  }
- 
-  return 0;
+    return 0;
 }
--- main.c
+++ main.c
@@ -46,7 +46,7 @@ int main() {
         
         t = clock() - t;
         double time_taken = ((double)t)/CLOCKS_PER_SEC * 1000;
-        msleep(1000/60 - time_taken); 
+        msleep(1000/60 - time_taken);
     }
 
     return 0;
